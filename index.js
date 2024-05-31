83a4fca253f8a2b83fdb020109cf5291362acabd
Sadman Sakib <Sadman@Aarongcs.com>
2024-05-31 18:56:12 +0600

    connect to database

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+// connect database //
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
+
+// middelwares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+// router middelware //
+// app.use(authRouters);
+
+const port = process.env.PORT || 3001;
+
+app.get("/",(req,res)=>{
+  res.send("Your are getting data")
+})
+
+app.listen(port, () => {
+  console.log(`This is running ${port}`);
+});
