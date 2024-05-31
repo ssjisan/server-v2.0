bbb988d9f644af3a690bfdbd5b5991054f47f20e
Sadman Sakib <Sadman@Aarongcs.com>
2024-05-31 20:21:07 +0600

    create

--- routes/albumRoutes.js
+++ routes/albumRoutes.js
@@ -1,5 +1,4 @@
 import express from "express";
-import multer from "multer";
 
 const router = express.Router();
 // import controller
@@ -10,13 +9,11 @@ import {
 // import middleware
 import { requiredSignIn, isAdmin } from "../middlewares/authMiddleware.js";
 
-const upload = multer({ dest: "uploads/" });
 
 router.post(
   "/album",
   requiredSignIn,
   isAdmin,
-  upload.array("images", 30),
   createAlbum
 );
 // router.get("/album/:slug", readAlbum);
